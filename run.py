e80383239d18609445707e55e04a82eb03d898f3
EMHMoloney <>
2023-11-15 20:54:44 +0000

    Fix for validation of input- check for length and numerical values

--- run.py
+++ run.py
@@ -22,42 +22,43 @@ def get_product_titles(shop_sales):
 
 weekly_shop_sales = {}
 
+
 def get_sales_input(product_titles):
     """
     Requests sale figures to be inputted from user,
     validates the data entered
     """
-    sales_input = input("Please enter this weeks SHOP sales for all items, seperated by commas - ")
-    sales_values = [int(sale) for sale in sales_input.split(',')]
+    while True:
+        sales_input = input("Please enter this weeks SHOP sales for all items, separated by commas - ")
+        try:
+            sales_values = [int(sale) for sale in sales_input.split(',')]
+        except ValueError:
+            print("Please input numbers only.")
+            continue
 
-    if len(sales_values) != len(product_titles):
-        print("Please enter a sale value for each item - no sales must be entered as 0.")
-        return None
+        if len(sales_values) != len(product_titles):
+            print("Please enter a sale value for each item - 'no sales' for an item is to be entered as 0.")
+            continue
 
-    try:
-        sales_values = [int(sale) for sale in sales_values]
-    except ValueError:
-        print("Sales must be entered as whole numbers, please try again ")
-        return None
+        return sales_values
 
-    return sales_values
 
 def update_shop_sales(shop_sales, product_titles, sales_values):
     """
     Updates thes sheet being worked on with
     validated sales figures from user
     """
-    weekly_shop_sales = {} 
+    weekly_shop_sales = {}
 
-    shop_sales.append_row(sales_values, value_input_option = 'USER_ENTERED')
+    shop_sales.append_row(sales_values, value_input_option='USER_ENTERED')
     weekly_shop_sales = dict(zip(product_titles, sales_values))
 
     return weekly_shop_sales
 
 
-def main ():
+def main():
     """
-    Calls functions, passes data to them 
+    Calls functions, passes data to them
     to execute program
     """
     shop_sales = SHEET.worksheet('shop-sales')
@@ -66,12 +67,7 @@ def main ():
     sales_values = get_sales_input(product_titles)
     if sales_values is not None:
         weekly_shop_sales = update_shop_sales(shop_sales, product_titles, sales_values)
-#etsy_data = etsy_sales.get_all_values()
-#print(etsy_data)
+        print(f"Weekly SHOP sales:  \n")
+        print(weekly_shop_sales)
 
-#shop_data = shop_sales.get_all_values()
-#print(shop_data)
-
-#print(f"Weekly SHOP sales:  \n")
-#print(weekly_shop_sales)
 main()
